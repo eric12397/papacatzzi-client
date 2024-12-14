1e31d9dadb29d22a5ac306987055a08fe79ebd95
Eric <eric12397@gmail.com>
2024-12-14 17:03:17 -0500

    updating types

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,4 +1,12 @@
-type Coordinates = {
+type PostCoordinates = {
+    PostID: number
     Latitude: number
     Longitude: number
+    Timestamp: number
+}
+
+type PostDetails = {
+    ID: number
+    Animal: string
+    Author: string
 }
