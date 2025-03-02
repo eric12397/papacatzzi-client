40995a40218db0e746c6b355a3f056dfcb69a9e5
Eric <eric12397@gmail.com>
2025-03-01 19:15:59 -0500

    created forgot password modal

---
+++ src/routes/(app)/forgot-password/+page.server.ts
@@ -0,0 +1,31 @@
+import { fail, redirect } from "@sveltejs/kit"
+import type { Actions } from "./$types"
+
+export const actions: Actions = {
+    default: async ({ request }) => {
+        const formData = await request.formData()
+		const email = formData.get('email') as string
+
+        // TODO: add validation
+		// if (!email) {
+		// 	return fail(400, { email, missing: true });
+		// }
+		
+		const body: ForgotPasswordRequest = {email}
+		const response = await fetch(`${import.meta.env.VITE_BASE_URL}/forgot-password`, {
+			method: "POST",
+			body:  JSON.stringify(body),
+		});
+		
+		if (!response.ok) {
+			const data = await response.json();	
+			return {
+				status: response.status,
+				success: false,
+				data
+			}
+		}
+
+        throw redirect(303, "/")
+    }
+}
---
+++ src/routes/(app)/forgot-password/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { enhance } from "$app/forms";
+	import { goto } from "$app/navigation";
+	import Modal from "$lib/components/Modal.svelte";
+
+    let email = $state("")
+</script>
+
+<Modal close={() => goto("/")}>
+    <div class="flex px-4 pt-4 justify-end">
+        <button
+            onclick={() => goto("/")}
+            aria-label="Close modal"
+            class="w-4 h-8"
+        >
+        <i class="fa-solid fa-x"></i>
+        </button>
+    </div>
+
+    <form 
+        method="POST" 
+        class="flex flex-col h-full pt-2 px-6 pb-6 md:px-24 md:pb-10"
+        use:enhance
+    >
+        <h2 class="text-xl font-bold mb-4">Reset your password</h2>
+
+        <p class="text-sm mb-4">Enter your email and we'll send you a link to reset your password.</p>
+
+        <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            class="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+            bind:value={email}
+        />
+
+        <div class="mt-auto">
+            <button
+                type="submit"
+                
+                disabled={email === ""}
+                class="w-full text-white py-2 px-4 rounded-full 
+                {email === "" ? 'bg-gray-200 text-gray-400' : 'bg-indigo-700'}"
+            >
+                Reset password
+            </button>
+        </div>
+    </form>
+</Modal>
